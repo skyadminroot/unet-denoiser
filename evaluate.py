a747613be9a6a8056bb1c35ceb39c9b2a342163b
chenpeng <chenpeng@nutstore.net>
2021-08-13 20:55:50 +0800

    optimize

--- evaluate.py
+++ evaluate.py
@@ -1,40 +1,37 @@
-import mxnet as mx
-from mxnet import image, nd
-import numpy as np
-from mxnet import nd, autograd
-from mxnet.gluon import data as gdata
-from model_unet import UNet
-from loader import SegDataset
+import argparse
+import time
+
 import cv2
 import matplotlib.pyplot as plt
+import mxnet as mx
+import numpy as np
+from mxnet import nd
 
-from mxnet.gluon import loss as gloss, data as gdata, utils as gutils
-import sys
-import time
-import numpy
-import argparse
+from model_unet import UNet
+
 
 # numpy.set_printoptions(threshold=sys.maxsize)
 
 def normalize_image(img, ctx):
     """normalize image for bitonal processing"""
-    if isinstance(ctx, list) :
+    if isinstance(ctx, list):
         raise Exception('Context should not be an collection')
     # rgb_mean = nd.array([0.94040672, 0.94040672, 0.94040672])
     # rgb_std = nd.array([0.14480773, 0.14480773, 0.14480773])
     # second augmented set
-    
+
     # rgb_mean = nd.array([0.93610591, 0.93610591, 0.93610591])
     # rgb_std = nd.array([0.1319155, 0.1319155, 0.1319155])
 
-    rgb_mean = nd.array([0.79801027,0.79801027, 0.79801027], ctx = ctx)
-    rgb_std = nd.array([0.33287712, 0.33287712, 0.33287712], ctx = ctx)
+    rgb_mean = nd.array([0.79801027, 0.79801027, 0.79801027], ctx=ctx)
+    rgb_std = nd.array([0.33287712, 0.33287712, 0.33287712], ctx=ctx)
 
     # rgb_mean = nd.array([0.0, 0.0, 0.0])
     # rgb_std = nd.array([1.0, 1.0, 1.0])
-    
+
     return (img.astype('float32') / 255.0 - rgb_mean) / rgb_std
     # return (img.astype('float32') / 255.0)
+
 
 def post_process_mask(pred, img_cols, img_rows, n_classes, p=0.5):
     """ 
@@ -45,10 +42,12 @@ def post_process_mask(pred, img_cols, img_rows, n_classes, p=0.5):
     # print(pred.asnumpy())
     return pred.asnumpy().reshape(img_rows, img_cols).astype('uint8')
 
+
 def showAndDestroy(label, image):
     cv2.imshow(label, image)
-    cv2.waitKey(0)           
-    cv2.destroyAllWindows() 
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 def load_network(network_parameters, ctx):
     # At one point this can be generalized but right now I don't see this changing 
@@ -56,9 +55,9 @@ def load_network(network_parameters, ctx):
     n_channels = 3
 
     # Setup network
-    net = UNet(in_channels = n_channels, num_class = n_classes)
+    net = UNet(in_channels=n_channels, num_class=n_classes)
     net.load_parameters(network_parameters, ctx=ctx)
-    
+
     return net
 
 
@@ -86,19 +85,19 @@ def recognize(network_parameters, image_path, ctx, debug):
     net = load_network(network_parameters, ctx)
     n_classes = 2
     n_channels = 64
-    
-    img_height = 128 
-    img_width = 352
+
+    img_height = 467
+    img_width = 679
     # img_height = 96 
     # img_width = 576
 
     # prepare images
-    src = cv2.imread(image_path) 
+    src = cv2.imread(image_path)
     # ratio, resized_img = resize_and_frame(src, height=512)
     resized_img = src
-    
-    img = mx.nd.array(resized_img)    
-    normal = normalize_image(img)
+
+    img = mx.nd.array(resized_img)
+    normal = normalize_image(img, ctx[0])
     name = image_path.split('/')[-1]
 
     if debug:
@@ -121,7 +120,7 @@ def recognize(network_parameters, image_path, ctx, debug):
     out = net(data)
     pred = mx.nd.argmax(out, axis=1)
     print(pred.shape)
-    nd.waitall() # Wait for all operations to finish as they are running asynchronously
+    nd.waitall()  # Wait for all operations to finish as they are running asynchronously
     mask = post_process_mask(pred, img_width, img_height, n_classes, p=0.5)
 
     print(mask.shape)
@@ -130,15 +129,16 @@ def recognize(network_parameters, image_path, ctx, debug):
     # ratio, rescaled_mask = image_resize(mask, height=rescaled_height)  
     # mask = rescaled_mask
     if debug:
-        ax4.imshow(mask, cmap=plt.cm.gray) 
+        ax4.imshow(mask, cmap=plt.cm.gray)
 
     dt = time.time() - start
     print('Eval time %.3f sec' % (dt))
     if debug:
         plt.show()
 
-    mask = mask * 255 # currently mask is 0 / 1 
+    mask = mask * 255  # currently mask is 0 / 1
     return src, mask
+
 
 def recognize_patch(net, ctx, image, shape):
     """Recognize form
@@ -159,15 +159,15 @@ def recognize_patch(net, ctx, image, shape):
     if isinstance(ctx, mx.Context):
         ctx = [ctx]
     start = time.time()
-    
+
     img_height = shape[0]
     img_width = shape[1]
     n_classes = 2
 
     # prepare images
     src = image
-    img = mx.nd.array(src, ctx = ctx[0])    
-    normal = normalize_image(img, ctx = ctx[0])
+    img = mx.nd.array(src, ctx=ctx[0])
+    normal = normalize_image(img, ctx=ctx[0])
     # Transform into required BxCxHxW shape
     data = np.transpose(normal, (2, 0, 1))
     # Exand shape into (B x H x W x c)
@@ -176,7 +176,7 @@ def recognize_patch(net, ctx, image, shape):
     # prediction 
     out = net(data)
     pred = mx.nd.argmax(out, axis=1)
-    nd.waitall() # Wait for all operations to finish as they are running asynchronously
+    nd.waitall()  # Wait for all operations to finish as they are running asynchronously
     mask = post_process_mask(pred, img_width, img_height, n_classes, p=0.5)
 
     # rescaled_height = int(img_height / ratio)
@@ -184,18 +184,21 @@ def recognize_patch(net, ctx, image, shape):
     # mask = rescaled_mask
     dt = time.time() - start
     print('Eval time %.3f sec' % (dt))
-    mask = mask * 255 # currently mask is 0 / 1 
+    mask = mask * 255  # currently mask is 0 / 1
     return src, mask
 
 
 def parse_args():
     """Parse input arguments"""
     parser = argparse.ArgumentParser(description='UNET evaluator')
-    parser.add_argument('--network-param', dest='network_param', help='Network parameter filename',default='unet_best.params', type=str)
-    parser.add_argument('--image', dest='img_path', help='Image filename to evaluate', default='data/input.png', type=str)
+    parser.add_argument('--network-param', dest='network_param', help='Network parameter filename',
+                        default='unet_best.params', type=str)
+    parser.add_argument('--image', dest='img_path', help='Image filename to evaluate', default='data/input.png',
+                        type=str)
     parser.add_argument('--debug', dest='debug', help='Debug results', default=False, type=bool)
 
     return parser.parse_args()
+
 
 def imwrite(path, img):
     try:
@@ -204,17 +207,31 @@ def imwrite(path, img):
     except Exception as ident:
         print(ident)
 
+
+# 将识别出的手写区域擦除掉
+def recoverImage(image, mask):
+    h = image.shape[0]
+    w = image.shape[1]
+    for i in range(0, h):
+        for j in range(0, w):
+            if mask[i, j] != 0:
+                image[i, j] = 255
+    pass
+
+
 if __name__ == '__main__':
     args = parse_args()
     args.network_param = './unet_best.params'
-    args.img_path = './data/test/image/000974.png'
+    args.img_path = './data/train/image/16.png'
     # args.img_path = '/home/gbugaj/devio/unet-denoiser/assets/snippets/snippet.png'
     # args.img_path = '/home/greg/dev/unet-denoiser/assets/cleaned-examples/field-set-01/07.png'    
-    args.debug = True
+    args.debug = False
 
     ctx = [mx.cpu()]
     src, mask = recognize(args.network_param, args.img_path, ctx, args.debug)
     name = args.img_path.split('/')[-1]
 
-    imwrite('/tmp/debug/%s_src.tif' % (name), src)
-    imwrite('/tmp/debug/%s_mask.tif' % (name), mask)
+    imwrite('./debug_out/%s_src.png' % (name), src)
+    imwrite('./debug_out/%s_mask.png' % (name), mask)
+    recoverImage(src, mask)
+    imwrite('./debug_out/%s_recover.png' % (name), src)
--- generate_database_v3.py
+++ generate_database_v3.py
@@ -38,7 +38,69 @@ def imWrite(path, img):
         pass
 
 
-def copy_handwrite_images(bg_image, files, dest_mask_dir, dest_image_dir, progress_bar):
+def rotate_bound(image, angle, scale):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, scale)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH), borderValue=(255, 255, 255))
+
+
+def overlayImage(bgp, mask_image):
+    # resize
+    if mask_image.shape[1] > bgp.shape[1]:
+        tW = int(bgp.shape[1] - 100)
+        tH = int(mask_image.shape[0] * tW / mask_image.shape[1])
+        mask_image = cv2.resize(mask_image, (tW, tH))
+
+    # resize
+    # bg:96*384
+    # mask:111*383
+    if mask_image.shape[0] > bgp.shape[0]:
+        tH = int(bgp.shape[0] - 10)
+        tW = int(mask_image.shape[1] * tH / mask_image.shape[0])
+        mask_image = cv2.resize(mask_image, (tW, tH))
+
+    width = mask_image.shape[1]
+    height = mask_image.shape[0]
+
+    start_x = 0
+    if bgp.shape[1] - width - 1 > 0:
+        start_x = numpy.random.randint(low=0, high=bgp.shape[1] - width - 1, size=1)[0]
+
+    start_y = 0
+    if bgp.shape[0] - height - 1 > 0:
+        start_y = numpy.random.randint(low=0, high=bgp.shape[0] - height - 1, size=1)[0]
+
+    for x in range(width):
+        for y in range(height):
+            if mask_image[y, x] != 255:
+                try:
+                    bgp[start_y + y, start_x + x] = mask_image[y, x]
+                except Exception as error:
+                    print(error)
+
+    return start_x, start_y, width, height
+
+
+def copy_handwrite_images(bg_image, all_img_list, files, dest_mask_dir, dest_image_dir, progress_bar):
     global mImageCount
     bg = cv2.imread(bg_image, cv2.IMREAD_GRAYSCALE)
     for idx, val in enumerate(files):
@@ -46,44 +108,19 @@ def copy_handwrite_images(bg_image, files, dest_mask_dir, dest_image_dir, progre
         dest_mask_name = os.path.join(dest_mask_dir, str(mImageCount) + ".png")
         dest_image_name = os.path.join(dest_image_dir, str(mImageCount) + ".png")
 
-        src = cv2.imread(files[idx], cv2.IMREAD_GRAYSCALE)
-        mask_image = cv2.threshold(src, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # 合成多个遮盖图片
+        sample_size = int(len(files) * 0.8)
+        if sample_size > 10:
+            sample_size = 10
+        random_image = random.sample(all_img_list, sample_size)
+        mask_image = np.ones(bg.shape, bg.dtype) * 255
+        for imgUrl in random_image:
+            img = cv2.imread(imgUrl, cv2.IMREAD_GRAYSCALE)
+            img = rotate_bound(img, random.randint(-180, 180), random.uniform(0.3, 0.6))
+            overlayImage(mask_image, img)
 
         bgp = bg.copy()
-
-        # resize
-        if mask_image.shape[1] > bgp.shape[1]:
-            tW = int(bgp.shape[1] - 100)
-            tH = int(mask_image.shape[0] * tW / mask_image.shape[1])
-            mask_image = cv2.resize(mask_image, (tW, tH))
-
-        # resize
-        # bg:96*384
-        # mask:111*383
-        if mask_image.shape[0] > bg.shape[0]:
-            tH = int(bg.shape[0] - 10)
-            tW = int(mask_image.shape[1] * tH / mask_image.shape[0])
-            mask_image = cv2.resize(mask_image, (tW, tH))
-
-        width = mask_image.shape[1]
-        height = mask_image.shape[0]
-
-        startX = 0
-        if bgp.shape[1] - width - 1 > 0:
-            startX = numpy.random.randint(low=0, high=bgp.shape[1] - width - 1, size=1)[0]
-
-        starty = 0
-        if bgp.shape[0] - height - 1 > 0:
-            starty = numpy.random.randint(low=0, high=bgp.shape[0] - height - 1, size=1)[0]
-
-        for x in range(width):
-            for y in range(height):
-                if mask_image[y, x] != 255:
-                    try:
-                        bgp[starty + y, startX + x] = mask_image[y, x]
-                    except Exception as error:
-                        print(error)
-
+        start_x, start_y, width, height = overlayImage(bgp, mask_image)
         imWrite(dest_image_name, bgp)
 
         # resize mask_image
@@ -95,7 +132,7 @@ def copy_handwrite_images(bg_image, files, dest_mask_dir, dest_image_dir, progre
         for x in range(width):
             for y in range(height):
                 try:
-                    target_mask[starty + y, startX + x] = tmp_mask_image[y, x]
+                    target_mask[start_y + y, start_x + x] = tmp_mask_image[y, x]
                 except Exception as error:
                     print(error)
                 pass
@@ -157,12 +194,12 @@ def generate_old():
     print("origin handwriting copy complete")
 
 
-def generate_data(bg_dir, img_dir):
+def generate_data(bg_dir, img_dir, random_mask_size):
     if not os.path.exists(bg_dir) or not os.path.exists(img_dir):
         print("resource dir not exist")
         return
 
-    data_dir = "./handwrite_data/data"
+    data_dir = "./data"
 
     train_image_dir = data_dir + "/train/image"
     train_mask_dir = data_dir + "/train/mask"
@@ -176,30 +213,82 @@ def generate_data(bg_dir, img_dir):
     img_file_list = readAllImageFiles(img_dir)
 
     # 测试用数据集
-    test_bg_file_list = bg_file_list[0:20]
+    test_bg_file_list = bg_file_list[0:5]
     # 训练用数据集
-    train_bg_file_list = bg_file_list[20:-1]
+    train_bg_file_list = bg_file_list[5: 40]
 
     if len(bg_file_list) == 0 or len(img_file_list) == 0:
         print("miss resource file")
         return
     # 将M个背景图片映射到N个手写图片
     # 构建测试数据集
-    with tqdm(total=len(test_bg_file_list) * 10) as progress_bar:
+    with tqdm(total=len(test_bg_file_list) * random_mask_size) as progress_bar:
         for bgFile in test_bg_file_list:
             random.seed(time.time())
-            img_slice = random.sample(img_file_list, 10)
-            copy_handwrite_images(bgFile, img_slice, test_mask_dir, test_image_dir, progress_bar)
+            img_slice = random.sample(img_file_list, random_mask_size)
+            copy_handwrite_images(bgFile, img_file_list, img_slice, test_mask_dir, test_image_dir, progress_bar)
 
     # 构建训练数据集
-    with tqdm(total=len(train_bg_file_list) * 10) as progress_bar:
+    with tqdm(total=len(train_bg_file_list) * random_mask_size) as progress_bar:
         for bgFile in train_bg_file_list:
             random.seed(time.time())
-            img_slice = random.sample(img_file_list, 10)
-            copy_handwrite_images(bgFile, img_slice, train_mask_dir, train_image_dir, progress_bar)
+            img_slice = random.sample(img_file_list, random_mask_size)
+            copy_handwrite_images(bgFile, img_file_list, img_slice, train_mask_dir, train_image_dir, progress_bar)
+
+
+snt = 0
+
+
+def splitImageVertical(dir):
+    global snt
+    if not os.path.exists(dir):
+        print(dir, "not exist")
+        return
+    for name in os.listdir(dir):
+        if name == '.DS_Store':
+            continue
+        file = os.path.join(dir, name)
+        # 过滤掉目录
+        if os.path.isdir(file):
+            continue
+        image = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        if image is None:
+            print("can't read file: " + file)
+            continue
+        image_height = image.shape[0]
+        sub_image_height = int(image_height / 2)
+        top_image = image[0:sub_image_height, :]
+        bottom_image = image[sub_image_height:, :]
+
+        snt += 1
+        top_image_path = os.path.join("./casia", str(snt) + ".png")
+        imWrite(top_image_path, top_image)
+
+        snt += 1
+        bottom_image_path = os.path.join("./casia", str(snt) + ".png")
+        imWrite(bottom_image_path, bottom_image)
+
+
+def resizeAllImage(dir):
+    if not os.path.exists(dir):
+        print(dir, "not exist")
+        return
+    files = readAllImageFiles(dir)
+    for file in files:
+        if 'DS_Store' in file:
+            continue
+        image = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        if image is None:
+            continue
+        w = int(image.shape[1] / 2)
+        h = int(image.shape[0] / 2)
+        image = cv2.resize(image, (w, h))
+        imWrite(file, image)
 
 
 if __name__ == '__main__':
     # generate_old()
-    generate_data('./pdf2image', '/Users/nutstore/awork/datasets/handwriting/casia/HWDB2.2Train_images')
+    generate_data('./casia', '/Users/nutstore/awork/datasets/handwriting/casia/HWDB2.2Train_images', 5)
+    resizeAllImage('./data')
+    # splitImageVertical('./pdf2image')
     pass
